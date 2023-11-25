875e04b7107e08a52f185cc78aeefd120110bd89
Adam Elfsborg <adam.elfsborg@mtek.se>
2023-11-25 14:55:48 +0100

    feat: init project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    let count: number = $state(0)
+
+    const doubleCount = () => {
+        return count * 2
+    }
+
+    let doubled: number = $derived(doubleCount())
+
+    const incroment = () => {
+        count += 1; 
+    }
+
+    $effect(() => {
+        console.log({count, doubled})
+    })
+</script>
+
+<div>
+    <button type="button" on:click={incroment} >
+        {count} * 2 = {doubled}
+    </button>
+</div>
